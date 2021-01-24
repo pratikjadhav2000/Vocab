7a3b7151689ddb8ed79a484804728c70c8a62cbf
pratikjadhav2000 <56275925+pratikjadhav2000@users.noreply.github.com>
2021-01-24 19:12:56 +0530

    extension of the index file is changed

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="homeimage.css">
+	<title>Welcome to Vocab</title>
+</head>
+<body>
+
+		<header>
+			<div class="main">
+				
+			<ul>
+				<li ><a class="active" href="#">HOME</a> </li>
+				<li><a href="#">CONTACTS</a> </li>
+				<li><a href="#">ABOUT US</a> </li>
+				<li><a href="#">LEARN MORE</a> </li>
+
+			</ul>
+				
+			</div>
+
+			<div class="title">
+				<h1>VOCAB</h1>
+			</div>
+
+			<div class="button">
+				<a href="loginmain.php" class="btn">LOG IN</a>
+				<a href="signupmain.php" class="btn">SIGN UP</a>
+				
+			</div>
+		</header>
+			
+		
+</body>
+</html>
